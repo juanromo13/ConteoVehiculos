63e106c7941027c3b62e3340fa096d63019e3541
juanromo13 <juanochiro1315@gmail.com>
2021-02-08 18:09:07 -0500

    2.0

--- vehicle_count_with_stream_y_db.py
+++ vehicle_count_with_stream_y_db.py
@@ -25,7 +25,7 @@ for row in rows:
     tiempo = row[0]
 
 # Load Yolo
-net = cv2.dnn.readNet("yolov3-320.weights", "yolov3-320.cfg")
+net = cv2.dnn.readNet("yolov3-tiny.weights", "yolov3-tiny.cfg")
 classes = []
 with open("coco.names", "r") as f:
     classes = [line.strip() for line in f.readlines()]
